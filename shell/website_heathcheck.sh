310e23bee9a80f0c327bc86d260dbb92f513e155
Christian Solje <christian.solje@aller.com>
2021-07-23 10:20:52 +0200

    Added a oneliner as well

--- shell/website_heathcheck.sh
+++ shell/website_heathcheck.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #HOSTDNS=`dig +short http.ingress.cms.pblgw.io | sort -n`
-HOSTDNS=`dig +short $1 | grep -v '\.$' | sort -n`
-for HOST in $HOSTDNS
-do
-    curl https://$HOST/login/index -k --header "Host: $1" -I
-done
+while read line; do curl -I -k -s https://$line/login/index --header "Host: $1"; done < <(dig +short $1 | grep -v '\.$' | sort -n)  | grep -E "HTTP|X-Nginx"
+#HOSTDNS=`dig +short $1 | grep -v '\.$' | sort -n`
+#for HOST in $HOSTDNS
+#do
+#    curl https://$HOST/login/index -k --header "Host: $1" -I
+#done
 
